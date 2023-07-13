81ed2fa5c922d2a7ef5a038bf9fce8a0746b34a8
pratikp151001 <121818515+pratikp151001@users.noreply.github.com>
2023-07-13 10:08:21 +0530

    commit_Changes

--- src/Components/settings/Integrations/index.tsx
+++ src/Components/settings/Integrations/index.tsx
@@ -5,9 +5,9 @@ export default function index(props: any) {
     const { InterigrationData } = props
     return (
         <>
-            <Row justify={'space-between'}>
+            <Row justify={'space-between'} gutter={16}>
                 {InterigrationData.map((item: any, index: any) => (
-                    <Col xs={24} md={12} lg={5} sm={24} style={{ marginBottom: '2%',textAlign:'left' }} >
+                    <Col xs={24} md={12} lg={6} sm={24} xl={6} style={{ marginBottom: '2%',textAlign:'left' }} >
                         <Card bordered={true} >
                             <Image preview={false} src={item?.logo} style={{display:'block'}} />
                             <span style={{marginBottom:'2%'}} >Company:</span>
